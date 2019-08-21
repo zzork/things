a8fe52ab36b494beed5880efd2bdb406258ad81a
zzork <54363741+zzork@users.noreply.github.com>
2019-08-21 15:05:19 +0100

    Create welcomeToJava.java

---
+++ HackerRank solutions/java/welcomeToJava.java
@@ -0,0 +1,13 @@
+/* You must print two lines of output:
+ *
+ * Print Hello, World. on the first line.
+ * Print Hello, Java. on the second line
+ */
+ 
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
