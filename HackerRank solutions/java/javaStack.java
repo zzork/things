9488c7f85202cf3caa98103de037a147cf27ccd1
zzork <54363741+zzork@users.noreply.github.com>
2019-08-21 15:09:53 +0100

    Update javaStack.java

--- HackerRank solutions/java/javaStack.java
+++ HackerRank solutions/java/javaStack.java
@@ -1,5 +1,7 @@
-/* A string containing only parentheses is balanced if the following is true: 1. if it is an empty string 2. if A and B are correct, 
- * AB is correct, 3. if A is correct, (A) and {A} and [A] are also correct.
+/* A string containing only parentheses is balanced if the following is true: 
+ *  	1. if it is an empty string 
+ *	2. if A and B are correct, AB is correct, 
+ *	3. if A is correct, (A) and {A} and [A] are also correct.
  *
  * Examples of some correctly balanced strings are: "{}()", "[{()}]", "({()})" 
  *
@@ -8,6 +10,7 @@
  * Given a string, determine if it is balanced or not.
  */
 
+import java.util.*;
 import java.util.*;
 class Solution{
 	
@@ -45,6 +48,5 @@ class Solution{
 		}		
 	}
 }
-
 
 
