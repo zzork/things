74257bed33a5d6b254b4952ace1a88ae35a6115a
zzork <54363741+zzork@users.noreply.github.com>
2019-10-05 16:43:07 +0100

    Create Tuples.py

---
+++ HackerRank solutions/Python/Basic Data Types/Tuples.py
@@ -0,0 +1,7 @@
+# Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers. Then compute and print the result 
+# of hash(t).
+
+if __name__ == '__main__':
+    n = int(input())
+    integer_tuple = tuple(map(int, input().split()))
+    print(hash(integer_tuple))
