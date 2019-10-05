360b9b89912f3815b308b505f13a01b922dd6211
zzork <54363741+zzork@users.noreply.github.com>
2019-10-05 14:54:32 +0100

    Create Solution.java

---
+++ HackerRank solutions/Algorithms/Diagonal Difference/Solution.java
@@ -0,0 +1,53 @@
+/* Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+ */
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int size = arr.size(), lToR = 0, rToL = 0;
+
+        for (int i = 0; i < size; i++) {
+            lToR += arr.get(i).get(i);
+            rToL += arr.get(i).get(size - i - 1);
+        }
+        return Math.abs(lToR - rToL);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
