d8907043e349735a5804991aa93b668eb08d276f
zzork <54363741+zzork@users.noreply.github.com>
2019-10-05 09:48:43 +0100

    Create Solution.java

---
+++ HackerRank solutions/java/Object Oriented Programming/Java Interface/Solution.java
@@ -0,0 +1,49 @@
+/* You are given an interface AdvancedArithmetic which contains a method signature int divisor_sum(int n). You need to write a class called 
+ * MyCalculator which implements the interface.
+ *
+ * divisorSum function just takes an integer as input and return the sum of all its divisors. For example divisors of 6 are 1, 2, 3 and 6, 
+ * so divisor_sum should return 12. The value of n will be at most 1000.
+ */
+ 
+import java.util.*;
+interface AdvancedArithmetic{
+  int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic {
+    public int divisor_sum(int n) {
+        int limit = (int)Math.sqrt(n);
+        int total = 0;
+
+        for (int i = 1; i <= limit; i++) {
+            if (n % i == 0) {
+                total += i;
+                if (n / i != i) total += n / i;
+            }
+        }
+        return total;
+    }
+}
+
+class Solution{
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+      	sc.close();
+    }
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
+    }
+}
+
