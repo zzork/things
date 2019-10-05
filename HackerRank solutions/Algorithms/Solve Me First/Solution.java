985cff4cf232eacf1878301524e4b2fa77abe8b0
zzork <54363741+zzork@users.noreply.github.com>
2019-10-05 14:47:57 +0100

    Create Solution.java

---
+++ HackerRank solutions/Algorithms/Solve Me First/Solution.java
@@ -0,0 +1,22 @@
+/* Complete the function solveMeFirst to compute the sum of two integers.
+ */
+ 
+import java.util.Scanner;
+
+public class Solution {
+
+    static int solveMeFirst(int a, int b) {
+      	return a + b;
+   }
+
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
+}
